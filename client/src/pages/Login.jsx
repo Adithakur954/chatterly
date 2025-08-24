ae14af65942dcb790209bc29d3403c5f1564ff98
Aditya Singh <singhaditya1627@gmail.com>
2025-08-24 10:25:19 +0530

    Refactor ChatContainer, RightSidebar, Login, and Profile components for improved structure and functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets';
 
 function Login() {
+
+  const [currentState, setCurrentState] = useState("signUp");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [bio, setBio] = useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+  const SubmitHandler = (e) => {
+    e.preventDefault();
+    if (currentState === "signUp" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
   return (
-    <div>
-      
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+
+      {/* -------- left -------- */}
+      <div className='flex items-center flex-col'>
+        <img src={assets.logo_icon} alt="" className='w-[min(30vw,250px)]' />
+        <p className='text-white text-4xl'>Chatterly</p>
+      </div>
+
+      {/* -------- right -------- */}
+
+      <form onSubmit={SubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>{currentState}
+          {isDataSubmitted && <img onClick={()=>setIsDataSubmitted(false)} src={assets.arrow_icon} className='w-5 cursor-pointer' alt="" />}
+        </h2>
+        {currentState === "signUp" && !isDataSubmitted && (
+          <input onChange={(e) => setFullName(e.target.value)} value={fullName}
+            type="text" placeholder='FullName' className='p-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500' />
+        )}
+        {!isDataSubmitted && (
+          <>
+            <input onChange={(e) => setEmail(e.target.value)} value={email}
+              type="email" placeholder='Enter your Email' className='p-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500' />
+            <input onChange={(e) => setPassword(e.target.value)} value={password}
+              type="password" placeholder='Enter your Password' className='p-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500' />
+          </>
+        )}
+
+        {currentState === "signUp" && isDataSubmitted && (
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio}
+            placeholder='Tell us about yourself' className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500' />
+        )}
+        <button type='submit' className='py-3 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-lg cursor-pointer'>
+          {currentState === "signUp" && isDataSubmitted ? "Create Account" : "Login"}
+        </button>
+        <div className='text-sm text-gray-300 flex items-center gap-2'>
+          <input type="checkbox" name="" id="" />
+          <p>Agree to terms and conditions</p>
+        </div>
+        <div className='flex flex-col gap-2 text-gray-200'>
+          {currentState === "signUp" ? (
+            <p>Already have an account? <span className='text-blue-500 cursor-pointer' onClick={() => { setCurrentState("Login"); setIsDataSubmitted(false) }}>Login</span></p>
+          ) : (
+            <p>Don't have an account? <span className='text-blue-500 cursor-pointer' onClick={() => setCurrentState("signUp")}>Sign Up</span></p>
+          )}
+        </div>
+      </form>
+
     </div>
   )
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import assets from '../assets/assets'
 
-function Profile() {
+const ProfilePage = () => {
+
+  const [selectedImg, setSelectedImg] = useState(null)
+  const navigate = useNavigate()
+  const [name, setName] = useState("Martin Johnson")
+  const [bio, setBio] = useState("Hi Everyone, I am Using QuickChat")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate('/')
+    // Handle form submission
+  }
+
   return (
-    <div>
-      
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg'>Profile details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input onChange={(e) => setSelectedImg(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 rounded-full ${selectedImg ? "ring-2 ring-blue-500" : ""}`} /> Upload The new Image
+          </label>
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" required placeholder='Your Name' className='p-2 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' name="" id="" />
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} required placeholder='Your Profile bio ' className='p-2 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500' rows={4}></textarea>
+          <button type='submit' className='bg-gradient-to-r from-blue-500 to-purple-500 text-white p-2 rounded-full text-lg cursor-pointer'>Save Changes</button>
+        </form>
+        <img src={assets.logo_icon} className='max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10' alt="" />
+      </div>
     </div>
   )
 }
 
-export default Profile
+export default ProfilePage
